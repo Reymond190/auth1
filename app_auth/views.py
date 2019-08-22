19c0605a691f9438f6e0c884c692e315b9fb5e85
joseph reymond <josephreymond2011@gmail.com>
2019-08-22 18:20:49 +0530

    images+add css+ functions optimized

--- app_auth/views.py
+++ app_auth/views.py
@@ -11,7 +11,6 @@ from datetime import datetime
 from requests.auth import HTTPBasicAuth
 from ipywidgets.embed import embed_minimal_html, embed_snippet
 import json
-
 from background_task import background
 import requests
 import gmaps
@@ -84,28 +83,64 @@ def AddDevice(request):
 #     idle= idle +1
 #     return idle
 
+
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 @login_required
 def map (request):
-    time2 = datetime.datetime.now()
-    time1 = time2 + timedelta(minutes=-5)
-    time1 = time1.strftime("%Y-%m-%d %H:%M:00")
-    time2 = time2.strftime("%Y-%m-%d %H:%M:00")
-    time1 = str(time1)
-    time2 = str(time2)
-    r1 = requests.get('https://lnt.tracalogic.co/api/ktrack/larsentoubro/2019-08-20 10:05:00/2019-08-20 10:10:00' ,
+
+    def get_api():
+        time2 = datetime.datetime.now()
+        time1 = time2 + timedelta(minutes=-5)
+        time1 = time1.strftime("%Y-%m-%d %H:%M:00")
+        time2 = time2.strftime("%Y-%m-%d %H:%M:00")
+        time1 = str(time1)
+        time2 = str(time2)
+        r1 = requests.get('https://lnt.tracalogic.co/api/ktrack/larsentoubro/2019-08-20 10:05:00/2019-08-20 10:10:00',
                           auth=HTTPBasicAuth('admin', 'admin'))
-    x1 = r1.json()
-    x2 = json.dumps(x1)
-    y1 = json.loads(x2)
-    df1 = json_normalize(y1["assetHistory"])
-    df1['serverTimeStamp'] = pd.to_datetime(df1['serverTimeStamp'])
-    df1 = df1.set_index('serverTimeStamp')
-    df1['eventTimeStamp'] = pd.to_datetime(df1['eventTimeStamp'])  # total no of vehicles
-    df1 = df1.drop_duplicates(['deviceImeiNo'], keep='first')  # NUMBER OF VEHICLES WITH UNIQUE DEVICEIMEINO/PLATENUMBER
-    df2 = df1.loc[(df1["engine"] == "ON") & (df1["speed"] > 0)]  # RUNNING VEHICLES
-    df3 = df1.loc[(df1["engine"] == "ON") & (df1["speed"] == 0)]  # IDLE VEHICLES
-    df4 = df1.loc[(df1["engine"] == "OFF") & (df1["speed"] == 0)]  # STOP_VEHICLES
+        x1 = r1.json()
+        x2 = json.dumps(x1)
+        y1 = json.loads(x2)
+        return y1
+
+    @background(schedule=60)
+    def reload_and_store():
+        f = open('venv/temp.json', 'w+')
+        if f.read() is not None:
+            f.truncate(0)
+            print('fcuk its running')
+        x = get_api()
+        json.dump(x, f)
+        f.close()
+
+
+    def get_temp():
+        f = open('venv/temp.json', 'r+')
+        content = f.read()
+        return content
+
+
+    def get_dataframe(y1):
+        df1 = json_normalize(y1["assetHistory"])
+        df1['serverTimeStamp'] = pd.to_datetime(df1['serverTimeStamp'])
+        df1 = df1.set_index('serverTimeStamp')
+        df1['eventTimeStamp'] = pd.to_datetime(df1['eventTimeStamp'])  # total no of vehicles
+        df1 = df1.drop_duplicates(['deviceImeiNo'],
+                                  keep='first')  # NUMBER OF VEHICLES WITH UNIQUE DEVICEIMEINO/PLATENUMBER
+        return df1
+
+
+
+    def filter_running(df):
+        df2 = df.loc[(df["engine"] == "ON") & (df["speed"] > 0)]  # RUNNING VEHICLES
+        return df2
+
+    def filter_idle(df):
+        df3 = df.loc[(df["engine"] == "ON") & (df["speed"] == 0)]  # IDLE VEHICLES
+        return df3
+
+    def filter_stop(df):
+        df4 = df.loc[(df["engine"] == "OFF") & (df["speed"] == 0)]  # STOP_VEHICLES
+        return df4
 
     def myfun1(po):
             lat_list = list(po["latitude"])
@@ -117,61 +152,97 @@ def map (request):
             data1 = embed_snippet(views=[fig])
             return data1
 
-    def listfun(plate):
-        df5 = df1.loc[df1["plateNumber"] == plate]
-        return myfun2(df5)
-
-    def myfun2(mo):
-            lat_list = list(mo["latitude"])
-            long_list = list(mo["longitude"])
-            gmaps.configure(api_key="AIzaSyDmXhcX8z4d4GxPxIiklwNvtqxcjZoWsWU")
-            fig = gmaps.figure()
-            markers = gmaps.marker_layer(list(zip(lat_list, long_list)))
-            fig.add_layer(markers)
-            data1 = embed_snippet(views=[fig])
-            return data1
+    def listfun(plate, df):
+        df5 = df.loc[df1["plateNumber"] == plate]
+        return myfun1(df5)
 
 
+    def change_frames(r):  # enter required dataframes in this function
+        p1 = myfun1(r)
+        listpl = r["plateNumber"]
+        listsp = r["speed"]
+        listdt = r["eventTimeStamp"].dt.strftime("%Y-%m-%d %I:%M:%S %p")
+        result = zip(listpl, listdt, listsp)
+        return p1, result
 
+    time2 = datetime.datetime.now()
+    time1 = time2 + timedelta(minutes=-5)
 
-
+    reload_and_store(schedule=10)
+    temp = get_temp()
+    y1 = json.loads(temp)
+    df1 = get_dataframe(y1)
+    df2 = filter_running(df1)
+    df3 = filter_idle(df1)
+    df4 = filter_stop(df1)
 
 
 
     if request.method == 'GET' and 'totalbutton' in request.GET:
-        p1 = myfun1(df1)
-        listpl = df1["plateNumber"]
-        listsp = df1["speed"]
-        listdt = df1["eventTimeStamp"].dt.strftime("%Y-%m-%d %I:%M:%S %p")
-        result = zip(listpl, listdt, listsp)
+        # temp = get_temp()
+        # y1 = json.loads(temp)
+        # df1 = get_dataframe(y1)
+        # p1 = myfun1(df1)
+        # listpl = df1["plateNumber"]
+        # listsp = df1["speed"]
+        # listdt = df1["eventTimeStamp"].dt.strftime("%Y-%m-%d %I:%M:%S %p")
+        # result = zip(listpl, listdt, listsp)
+        # temp = get_temp()
+        # y1 = json.loads(temp)
+        # df1 = get_dataframe(y1)
+        p1, result = change_frames(df1)
     elif request.method == 'GET' and 'runningbutton' in request.GET:
-        p1 = myfun1(df2)
-        listpl = df2["plateNumber"]
-        listsp = df2["speed"]
-        listdt = df2["eventTimeStamp"].dt.strftime("%Y-%m-%d %I:%M:%S %p")
-        result = zip(listpl, listdt, listsp)
+        # p1 = myfun1(df2)
+        # listpl = df2["plateNumber"]
+        # listsp = df2["speed"]
+        # listdt = df2["eventTimeStamp"].dt.strftime("%Y-%m-%d %I:%M:%S %p")
+        # result = zip(listpl, listdt, listsp)
+        # temp = get_temp()
+        # y1 = json.loads(temp)
+        # df1 = get_dataframe(y1)
+        # df2 = filter_running(df1)
+        p1, result = change_frames(df2)
+
     elif request.method == 'GET' and 'idlebutton' in request.GET:
-        p1 = myfun1(df3)
-        listpl = df3["plateNumber"]
-        listsp = df3["speed"]
-        listdt = df3["eventTimeStamp"].dt.strftime("%Y-%m-%d %I:%M:%S %p")
-        result = zip(listpl, listdt, listsp)
+        # p1 = myfun1(df3)
+        # listpl = df3["plateNumber"]
+        # listsp = df3["speed"]
+        # listdt = df3["eventTimeStamp"].dt.strftime("%Y-%m-%d %I:%M:%S %p")
+        # result = zip(listpl, listdt, listsp)
+        # temp = get_temp()
+        # y1 = json.loads(temp)
+        # df1 = get_dataframe(y1)
+        # df3 = filter_idle(df1)
+        p1, result = change_frames(df3)
     elif request.method == 'GET' and 'stopbutton' in request.GET:
-        p1 = myfun1(df4)
-        listpl = df4["plateNumber"]
-        listsp = df4["speed"]
-        listdt = df4["eventTimeStamp"].dt.strftime("%Y-%m-%d %I:%M:%S %p")
-        result = zip(listpl, listdt, listsp)
+        # p1 = myfun1(df4)
+        # listpl = df4["plateNumber"]
+        # listsp = df4["speed"]
+        # listdt = df4["eventTimeStamp"].dt.strftime("%Y-%m-%d %I:%M:%S %p")
+        # result = zip(listpl, listdt, listsp)
+        # temp = get_temp()
+        # y1 = json.loads(temp)
+        # df1 = get_dataframe(y1)
+        # df4 = filter_stop(df1)
+        p1, result = change_frames(df4)
+
     else:
-        p1 = myfun1(df1)
-        listpl = df1["plateNumber"]
-        listsp = df1["speed"]
-        listdt = df1["eventTimeStamp"].dt.strftime("%Y-%m-%d %I:%M:%S %p")
-        result = zip(listpl, listdt, listsp)
+        # p1 = myfun1(df1)
+        # listpl = df1["plateNumber"]
+        # listsp = df1["speed"]
+        # listdt = df1["eventTimeStamp"].dt.strftime("%Y-%m-%d %I:%M:%S %p")
+        # result = zip(listpl, listdt, listsp)
+        # temp = get_temp()
+        # y1 = json.loads(temp)
+        # df1 = get_dataframe(y1)
+        p1, result = change_frames(df1)
 
     if request.method == 'POST' and 'listbutton' in request.POST:
+        # temp = get_temp()
+        # y1 = json.loads(temp)
+        # df1 = get_dataframe(y1)
         plate = request.POST['listbutton']
-        p1 = listfun(plate)
+        p1 = listfun(plate, df1)
         print(plate)
     else:
         print('escaped if case sorry!!!!')
